32fad4fc45866e20df029bd7a4aa137179e09cdf
Max Grover <maxi2312@gmail.com>
2022-03-22 16:25:17 -0500

    Update print_env_var.py

--- print_env_var.py
+++ print_env_var.py
@@ -2,7 +2,7 @@ import os
 
 var = os.getenv('SAMPLE_SECRET')
 username = os.getenv('ARM_USERNAME')
-token = os.get_env('ARM_TOKEN')
+token = os.getenv('ARM_TOKEN')
 
 print(var)
 print(type(username))
