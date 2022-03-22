dd016218f1802c676edf51065cd8c86ea6402743
Max Grover <maxi2312@gmail.com>
2022-03-22 16:32:37 -0500

    Add act io test

--- print_env_var.py
+++ print_env_var.py
@@ -1,4 +1,5 @@
 import os
+import act
 
 var = os.getenv('SAMPLE_SECRET')
 username = os.getenv('ARM_USERNAME')
@@ -7,3 +8,6 @@ token = os.getenv('ARM_TOKEN')
 print(var)
 print(type(username))
 print(type(token))
+
+files = act.discovery.download_data(username, token, 'sgpceilC1.b1', '2017-01-14', '2017-01-19')
+print(len(files))
